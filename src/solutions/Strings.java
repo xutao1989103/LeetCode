d636a374b7690c17ef41d4bf1e9ce6126ab9867f
xutao1989103 <xutao1989103@gmail.com>
2015-03-02 14:52:35 +0800

    Strings

--- src/solutions/Strings.java
+++ src/solutions/Strings.java
@@ -1,7 +1,6 @@
 package solutions;
 
-import java.util.Arrays;
-import java.util.Stack;
+import java.util.*;
 
 /**
  * Created by 431 on 2014/12/20.
@@ -78,7 +77,7 @@ public class Strings {
         while (start <= end) {
             while (!isalpha(s.charAt(start)) && start < len - 1) start++;
             while (!isalpha(s.charAt(end)) && end > 0) end--;
-            if(start>=end) return true;
+            if (start >= end) return true;
             if (Character.toLowerCase(s.charAt(start)) == Character.toLowerCase(s.charAt(end))) {
                 start++;
                 end--;
@@ -94,10 +93,109 @@ public class Strings {
         return false;
     }
 
+    public static int atoi(String str) {
+        if (str == null || str.length() == 0) return 0;
+        char[] content = str.toCharArray();
+        int length = str.length();
+        int start = 0;
+        int flag = 1;
+        int value = 0;
+        int max = Integer.MAX_VALUE / 10;
+        while (start < length) {
+            if (content[start] == ' ') {
+                start++;
+            } else {
+                break;
+            }
+        }
+
+        if (content[start] == '-') {
+            flag = -1;
+            start++;
+        } else if (content[start] == '+') {
+            start++;
+        }
+        while (start < length) {
+            if (content[start] >= '0' && content[start] <= '9') {
+                value = value * 10 + content[start] - 48;
+            } else {
+                break;
+            }
+            start++;
+        }
+        return flag * value;
+    }
+
+    public static String simplifyPath(String path) {
+        String[] paths = path.split("/+");
+        Stack<String> stack = new Stack<String>();
+        for (String s : paths) {
+
+            if (s.length() > 0) {
+                if (s.equals("..")) {
+                    if (stack.empty()) {
+                        continue;
+                    }
+                    stack.pop();
+                } else if (s.equals(".")) {
+                    continue;
+                } else {
+                    stack.push(s);
+                }
+            }
+
+        }
+        String result = "";
+        while (!stack.empty()) {
+            result = "/" + stack.pop() + result;
+        }
+        if (result.length() == 0) {
+            result += "/";
+        }
+        return result;
+    }
+
+
+    public static int romanToInt(String s) {
+        HashMap<Character, Integer> value;
+        value = new HashMap<Character, Integer>();
+        value.put('M', 1000);
+        value.put('D', 500);
+        value.put('C', 100);
+        value.put('L', 50);
+        value.put('X', 10);
+        value.put('V', 5);
+        value.put('I', 1);
+        int sum = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (i < s.length() - 1 && value.get(s.charAt(i)) < value.get(s.charAt(i + 1)))
+                sum -= value.get(s.charAt(i));
+            else
+                sum += value.get(s.charAt(i));
+        }
+        return sum;
+    }
+
+    public static String reverseWords(String s) {
+        if (s == null || s.length() == 0) return "";
+        String[] strings = s.split(" +");
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = strings.length - 1; i >= 0; i--) {
+            if (!strings[i].equals("")) {
+                stringBuilder.append(strings[i] + " ");
+            }
+        }
+        return stringBuilder.toString().trim();
+    }
+
+    public static List<String> restoreIpAddresses(String s) {
+        List<String> result = new ArrayList<String>();
+        return result;
+    }
+
     public static void main(String[] args) {
 
         boolean bResult = isPalindrome(".,");
-
-        System.out.println(bResult);
+        System.out.println(restoreIpAddresses("25525511135"));
     }
 }
